b50dd99f5ce66dcd410272ff9b9d614b792f4b47
antaldaniel <antaldaniel@gmail.com>
2022-12-16 22:29:07 +0100

    extended codebook_create

--- R/codebook_create.R
+++ R/codebook_create.R
@@ -4,7 +4,13 @@
 #' @details Use \code{var_code_table} for a single variable and \code{codebook_create}
 #' for a dataset.
 #' @param df A survey dataset.
-#' @return A data frame containing the original survey identifier,
+#' @param var_labels Should the variable labels be included in the codebook?
+#' Defaults to \code{TRUE}.
+#' @param val_labels Should the value labels be included in the codebook?
+#' Defaults to \code{TRUE}.
+#' @param freq Should a label frequency table be included in the codebook?
+#' Defaults to \code{FALSE.}
+#' @return A dataset (data frame) containing the original survey identifier,
 #' the variable names, the variable and value labels.
 #' @importFrom purrr reduce
 #' @importFrom declared is.declared
@@ -14,12 +20,30 @@
 #'                    package = "eurobarometer"))
 #'
 #' var_code_table(sample_data, "d7")
-#' codebook_create(sample_data[, c("d25", "d60")])
+#' codebook_create(sample_data[, c("d25", "d60")], freq=TRUE)
 #' @export
 
-codebook_create <- function(df) {
-  codebooks <- lapply(names(df), function(x) var_code_table(df, x))
-  purrr::reduce(codebooks, rbind)
+codebook_create <- function(df,
+                            var_labels = TRUE,
+                            val_labels = TRUE,
+                            freq = FALSE) {
+  codebooks <- lapply(names(df), function(x) var_code_table(df, x, var_labels=var_labels, val_labels=val_labels, freq=freq))
+  codebook <- purrr::reduce(codebooks, rbind)
+  survey_title <- dataset::dataset_title(df)[[1]][1]
+  survey_title <- ifelse (is.null(survey_title), "Unknown Survey", survey_title)
+
+  return_ds <- dataset::dataset(codebook,
+                   Attributes =  names(codebook)[which(! names(codebook) %in% c("freq", "rel_freq"))],
+                   Measures =  names(codebook)[which(names(codebook) %in% c("freq", "rel_freq"))],
+                   Title = paste0("Codebook for ", survey_title),
+                   Identifier = codebook$survey[1])
+
+  relitem <- dataset::related_item(Identifier = codebook$survey[1],
+                        relatedIdentifierType = "DOI", relationType = "IsDerivedFrom",
+                        resourceTypeGeneral = "Dataset")
+  attr(return_ds, "RelatedIdentifier") <- relitem
+
+  return_ds
 }
 
 
@@ -29,7 +53,11 @@ codebook_create <- function(df) {
 #' @importFrom dataset identifier
 #' @export
 
-var_code_table <- function(df, var_name) {
+var_code_table <- function(df,
+                           var_name,
+                           var_labels = TRUE,
+                           val_labels = TRUE,
+                           freq = FALSE) {
 
   identifier <- dataset::identifier(df)
 
@@ -42,30 +70,56 @@ var_code_table <- function(df, var_name) {
          var_name, "' is not among names(df).")
   }
 
-  if(declared::is.declared(df[[var_name]])){
-    ## var is of type 'declared'
-    var_labelling <- labels(df[[var_name]])
-    df_rows <- length(var_labelling)
-    data.frame(
-      survey = rep(as.character(unlist(identifier)), df_rows),
-      var_name_orig = rep(var_name, df_rows),
-      var_label_orig =  rep(attr(df[[var_name]], "label"), df_rows),
-      val_code_orig = as.integer(labels(df[[var_name]])),
-      val_label_orig = names(labels(df[[var_name]]))
-    )
-  } else {
-    ## var is not of type 'declared'
-    var_label = ifelse(is.null(attr(df[[var_name]], "label")),
-                       yes = NA_character_, no = attr(df[[var_name]], "label"))
-    data.frame(
-      survey = as.character(unlist(identifier)),
-      var_name_orig = var_name,
-      var_label_orig =  var_label,
-      val_code_orig = NA_integer_,
-      val_label_orig = NA_character_
-    )
+  var_label = ifelse(is.null(attr(df[[var_name]], "label")),
+                     yes = NA_character_,
+                     no = attr(df[[var_name]], "label"))
+
+  if(var_labels & ! val_labels) {
+    return_df <- data.frame ( var_name_orig = var_name,
+                              var_label_orig = as.character(var_label))
+    return(return_df)
   }
+
+  if ( var_labels & val_labels ) {
+    if(declared::is.declared(df[[var_name]])){
+      ## var is of type 'declared'
+      var_labelling <- labels(df[[var_name]])
+      df_rows <- length(var_labelling)
+      return_df <- data.frame(
+        survey = rep(as.character(unlist(identifier)), df_rows),
+        var_name_orig = rep(var_name, df_rows),
+        var_label_orig =  rep(attr(df[[var_name]], "label"), df_rows),
+        val_code_orig = as.integer(labels(df[[var_name]])),
+        val_label_orig = names(labels(df[[var_name]]))
+      )
+    } else {
+      ## var is not of type 'declared'
+      return_df <- data.frame(
+        survey = as.character(unlist(identifier)),
+        var_name_orig = var_name,
+        var_label_orig =  var_label,
+        val_code_orig = NA_integer_,
+        val_label_orig = NA_character_
+      )
+    }
+  }
+
+  if (freq) {
+    var_freq_table <- create_freq_table(df[[var_name]])
+    dplyr::left_join (return_df, var_freq_table, by = 'val_label_orig')
+  } else {return_df}
 }
 
-
-
+#' @keywords internal
+create_freq_table <- function(x) {
+  factor_var <- as.factor(declared::undeclare(x))
+  freq_table <- data.frame(
+    val_label_orig = names(summary(factor_var)),
+    freq = as.numeric(summary(factor_var))
+  )
+  freq_table$rel_freq <- freq_table$freq / sum(freq_table$freq)
+  if ( ! length(x) == sum(freq_table$freq) ) {
+    stop ("create_freq_table(x): frequencies and length(x) do not match.")
+  }
+  freq_table
+}
--- tests/testthat/test-codebook_create.R
+++ tests/testthat/test-codebook_create.R
@@ -3,9 +3,9 @@ sample_data <- read_sav_gesis(
                      package = "eurobarometer"))
 
 df <- sample_data
-var_code_table(df, var_name = "d60")
+var_code_table(df, var_name = "d60", freq = TRUE)
+names(df)
 cb <- codebook_create(df)
-names(cb)
 
 test_that("var_code_table()", {
   expect_equal(var_code_table(df, var_name = "d25")$survey
@@ -13,6 +13,15 @@ test_that("var_code_table()", {
 })
 
 test_that("codebook_create()", {
+  expect_equal(dataset::dataset_title(cb)$Title, "Codebook for Eurobarometer 82.4 (November-December 2014)")
+  expect_equal(attr(cb, "RelatedIdentifier")$Identifier,"doi:10.4232/1.13044")
+  expect_true(dataset::is.dataset(df))
   expect_true(all(names(df) %in% cb$var_name_orig))
   expect_equal(names(cb), c("survey", "var_name_orig", "var_label_orig", "val_code_orig", "val_label_orig"))
 })
+
+cbx <- codebook_create(df, freq=TRUE)
+test_that("codebook_create(df, freq=TRUE)", {
+  expect_equal(cbx$rel_freq[2:3], c(1,1))
+})
+
